9004162a35c5db0af421fee8c35c2c54fd17dd15
Mars Xue <marsxue0221@gmail.com>
2018-09-03 23:05:46 +1000

    add rotate sun

---
+++ Assets/SunControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunControl : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
+
+        transform.LookAt(Vector3.zero);
+	}
+}
